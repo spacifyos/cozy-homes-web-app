93c3d5bb297f9753219cd5e48057663161ac93ac
hz <haozhe@optimumtech.my>
2024-02-15 18:00:31 +0800

    setup http service

---
+++ src/services/httpUtilities/httpFunctionErrorResponse.js
@@ -0,0 +1,31 @@
+import _ from "lodash";
+import Toast from "@/src/utils/Toast";
+export const httpFunctionErrorResponse = (
+  err,
+  ignoreToast = false,
+) => {
+  const response = _.get(err, ["response"], "");
+  const message = _.get(err, ["message"], "");
+  const statusCode = _.get(response, "status", null);
+
+  // handle api error message
+  if (!_.isEmpty(response)) {
+    const messages = _.get(response, ["data", "message"], "");
+
+    if (!_.isEmpty(messages) && !ignoreToast) {
+      Toast.error(messages);
+    } else {
+      const unknownErrorMsg = `Unknown ${statusCode} Error`;
+      Toast.error(unknownErrorMsg);
+    }
+
+    return;
+  }
+
+  if (!_.isEmpty(message)) {
+    !ignoreToast ? Toast.error(message) : false;
+  } else {
+    const unknownErrorMsg = `Unknown ${statusCode} Error`;
+    Toast.error(unknownErrorMsg);
+  }
+};
