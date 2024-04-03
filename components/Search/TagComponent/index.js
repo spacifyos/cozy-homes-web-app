29a5b2af4c63141dcace55b949c95b4f0dae947b
hz <haozhe@optimumtech.my>
2024-04-03 13:14:59 +0800

    enhance UI

--- components/Search/TagComponent/index.js
+++ components/Search/TagComponent/index.js
@@ -3,7 +3,7 @@ import _ from "lodash";
 
 const TagComponent = ({ lists, onClickSelectTag }) => {
   return (
-    <div className="flex tag-container pb-3 bg-transparent mr-2">
+    <div className="flex tag-container pb-3 bg-transparent">
       {_.map(lists, (list) => {
         const name = _.get(list, ["name"], "");
         const isActive = _.get(list, ["isActive"], "");
