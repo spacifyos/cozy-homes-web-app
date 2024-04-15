075e72aa522ce38fdb8a594aad570e3ca361eba5
kianlong <kianlong@optimumtech.my>
2024-04-15 11:31:31 +0800

    property page added description and fix the tenancy modal coding

--- components/MyTenancy/E-AgreementSection/index.js
+++ components/MyTenancy/E-AgreementSection/index.js
@@ -16,7 +16,7 @@ const Eagreement = ({t}) => {
                     <CustomText textClassName="font-size-xxsmall disable-text">
                         Status
                     </CustomText>
-                    <StatusLabel status="pending"/>
+                    <StatusLabel status="Pending"/>
                 </div>
             </div>
 
@@ -32,7 +32,109 @@ const Eagreement = ({t}) => {
             />
             <CustomLabelValue value="19 Aug 2023" label="Agreement Date"/>
             <CustomLabelValue value="19 Aug 2023 -18 Aug 2023" label="Tenure"/>
+            <div
+                className="divider-line"
+                style={{marginTop: 10, marginBottom: 10}}
+            ></div>
 
+            <CustomLabelValue value="M Vertica" label="Landlord"/>
+
+            <div className="pb-2">
+                <CustomText textClassName="font-size-xxsmall disable-text">
+                    Activity
+                </CustomText>
+                <div className="pt-1 grid grid-cols-2 gap-2">
+                    <div className="flex mr-3 items-start">
+                        <CustomImage
+                            src={Images.checkGreyIcon}
+                            className="mr-1"
+                            height={18}
+                            width={18}
+                        />
+                        <div className="flex flex-col">
+                            <CustomText textClassName="font-size-small disable-text">
+                                Agreed
+                            </CustomText>
+                            <CustomText textClassName="font-size-xxsmall disable-text">
+                                on 19 Aug 03:46 pm
+                            </CustomText>
+                        </div>
+                    </div>
+                    <div className="flex items-start">
+                        <CustomImage
+                            src={Images.checkGreyIcon}
+                            className="mr-1"
+                            height={18}
+                            width={18}
+                        />
+                        <div className="flex flex-col">
+                            <CustomText textClassName="font-size-small disable-text">
+                                Signed
+                            </CustomText>
+                            <CustomText textClassName="font-size-xxsmall disable-text">
+                                on 19 Aug 03:46 pm
+                            </CustomText>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div
+                className="divider-line"
+                style={{marginTop: 10, marginBottom: 10}}
+            ></div>
+
+            <CustomLabelValue value="John Doe" label="Tenant"/>
+
+            <div className="pb-2">
+                <CustomText textClassName="font-size-xxsmall disable-text">
+                    Activity
+                </CustomText>
+                <div className="pt-1 grid grid-cols-2 gap-2">
+                    <div className="flex mr-3 items-start">
+                        <CustomImage
+                            src={Images.checkGreyIcon}
+                            className="mr-1"
+                            height={18}
+                            width={18}
+                        />
+                        <div className="flex flex-col">
+                            <CustomText textClassName="font-size-small disable-text">
+                                Agreed
+                            </CustomText>
+                            <CustomText textClassName="font-size-xxsmall disable-text">
+                                on 19 Aug 03:46 pm
+                            </CustomText>
+                        </div>
+                    </div>
+                    <div className="flex items-start">
+                        <CustomImage
+                            src={Images.checkGreyIcon}
+                            className="mr-1"
+                            height={18}
+                            width={18}
+                        />
+                        <div className="flex flex-col">
+                            <CustomText textClassName="font-size-small disable-text">
+                                Signed
+                            </CustomText>
+                            <CustomText textClassName="font-size-xxsmall disable-text">
+                                on 19 Aug 03:46 pm
+                            </CustomText>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div
+                className="divider-line"
+                style={{marginTop: 10, marginBottom: 10}}
+            ></div>
+
+            <div className="grid grid-cols-2 gap-2">
+                <CustomLabelValue value="Pending" label="Stamping Status"/>
+                <CustomLabelValue value="No" label="Insurance"/>
+            </div>
 
 
         </div>
