7c81dad4b38d55d1307e13ee831598024bb4a006
hz <haozhe@optimumtech.my>
2024-05-30 15:25:00 +0800

    update selector

--- src/selectors/invoice.js
+++ src/selectors/invoice.js
@@ -1,13 +1,30 @@
 import _ from "lodash";
 
 export const getInvoiceListingData = (state) =>
-    _.get(state, ["invoice", "tenancyListing", "data"], null);
+  _.get(state, ["invoice", "tenancyListing", "data"], null);
 export const getInvoiceListingPagination = (state) =>
-    _.get(state, ["invoice", "tenancyListing", "pagination"], null);
+  _.get(state, ["invoice", "tenancyListing", "pagination"], null);
 export const getInvoiceListingLoading = (state) =>
-    _.get(state, ["invoice", "tenancyListing", "loading"], false);
+  _.get(state, ["invoice", "tenancyListing", "loading"], false);
 
 export const getInvoiceOverviewData = (state, id) =>
-    _.get(state, ["invoice", "tenancyOverview", id, "data"], null);
+  _.get(state, ["invoice", "tenancyOverview", id, "data"], null);
 export const getInvoiceOverviewLoading = (state) =>
-    _.get(state, ["invoice", "tenancyOverview", "loading"], false);
+  _.get(state, ["invoice", "tenancyOverview", "loading"], false);
+
+export const getInvoiceNumber = (state) => _.get(state, ["invoice_number"], "");
+export const getStatus = (state) => _.get(state, ["status"], "");
+export const getIsPaid = (state) => _.get(state, ["is_paid"], "");
+export const getRentalFee = (state) => _.get(state, ["rental_fee"], "");
+export const getDueDate = (state) => _.get(state, ["due_date"], "");
+export const getUpdatedAt = (state) => _.get(state, ["updated_at"], "");
+export const getTotalPay = (state) => _.get(state, ["total_pay"], "");
+export const getOverdue = (state) => _.get(state, ["overdue"], "");
+export const getDueSoon = (state) => _.get(state, ["due_Soon"], "");
+export const getBillTo = (state) => _.get(state, ["bill_to"], "");
+export const getProperty = (state) => _.get(state, ["property"], "");
+export const getTenancyCode = (state) => _.get(state, ["tenancy_code"], "");
+export const getSchedule = (state) => _.get(state, ["schedule"], "");
+export const getSubtotal = (state) => _.get(state, ["subtotal"], "");
+export const getTax = (state) => _.get(state, ["tax"], "");
+export const getTotalAmount = (state) => _.get(state, ["total_amount"], "");
--- src/selectors/tenancy.js
+++ src/selectors/tenancy.js
@@ -9,3 +9,12 @@ export const getTenancyOverviewData = (state, id) =>
   _.get(state, ["tenancy", "tenancyOverview", id, "data"], null);
 export const getTenancyOverviewLoading = (state) =>
   _.get(state, ["tenancy", "tenancyOverview", "loading"], false);
+
+export const getName = (state) => _.get(state, ["name"], "");
+export const getAddress = (state) => _.get(state, ["address"], "");
+export const getTenancyCode = (state) => _.get(state, ["tenancy_code"], "");
+export const getTenancyPeriod = (state) => _.get(state, ["tenancy_period"], "");
+export const getRentalFee = (state) => _.get(state, ["rental_fee"], "");
+export const getTenancyRemainingDay = (state) =>
+  _.get(state, ["tenancy_remaining_day"], "");
+export const getCreatedAt = (state) => _.get(state, ["created_at"], "");
