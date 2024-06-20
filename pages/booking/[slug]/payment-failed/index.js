b3c69dc318bc44504553d2b289b1aa1a71eeaddb
hz <haozhe@optimumtech.my>
2024-06-20 09:54:36 +0800

    enhancement

---
+++ pages/booking/[slug]/payment-failed/index.js
@@ -0,0 +1,58 @@
+import CustomText from "@/components/CustomText";
+import CustomImage from "@/components/CustomImage";
+import Images from "@/src/utils/Image";
+import { useRouter } from "next/router";
+import CustomButton from "@/components/CustomButton";
+import { useTranslation, withTranslation } from "next-i18next";
+import { getServerSideProps } from "@/src/utils/getStatic";
+
+export { getServerSideProps };
+
+const PaymentFailed = ({ id }) => {
+  const router = useRouter();
+  const { t } = useTranslation("common");
+
+  const onClickGoMainPage = () => {
+    router.replace("/explore");
+  };
+
+  const onClickPaymentFail = () => {
+    router.replace(`/booking/${id}/overview`);
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center pt-32 relative">
+      <CustomImage
+        src={Images.cancelIcon}
+        imageStyle={{ width: "20px" }}
+        className=" absolute top-5 right-5 cursor-pointer"
+        onClick={onClickGoMainPage}
+      />
+
+      <CustomImage
+        src={Images.failIcon}
+        imageStyle={{ width: "150px", height: "150px" }}
+      />
+
+      <CustomText textClassName="font-bold pt-4" styles={{ fontSize: "25px" }}>
+        {t("payment.paymentFailed")}
+      </CustomText>
+
+      <div className="pb-4 px-10 pt-4">
+        <CustomText textClassName="font-size-xsmall text-center">
+          Sorry. We encounter an error while processing your payment. Please try
+          again later.
+        </CustomText>
+      </div>
+
+      <CustomButton
+        buttonClassName="primary-btn"
+        buttonStyles={{ padding: "5px 30px" }}
+        buttonText={t("payment.viewBooking")}
+        onClick={onClickPaymentFail}
+      />
+    </div>
+  );
+};
+
+export default PaymentFailed;
