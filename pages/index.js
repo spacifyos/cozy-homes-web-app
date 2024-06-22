66100cd745ada3453139487d7294fe4599b64eb7
hz <haozhe@optimumtech.my>
2024-06-22 15:14:38 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,8 @@ import { withTranslation, useTranslation } from "next-i18next";
 import { getServerSideProps } from "@/src/utils/getStatic";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import _ from "lodash";
-import AuthManager from "@/src/utils/AuthManager";
-import Toast from "@/src/utils/Toast";
 import CustomImage from "@/components/CustomImage";
 import Images from "@/src/utils/Image";
-import CustomText from "@/components/CustomText";
 
 export { getServerSideProps };
 
@@ -16,11 +12,11 @@ function Home() {
   const { t } = useTranslation("common");
   const router = useRouter();
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     router.replace("/explore");
-  //   }, 1000);
-  // });
+  useEffect(() => {
+    setTimeout(() => {
+      router.replace("/explore");
+    }, 1000);
+  });
 
   return (
     <div
@@ -34,10 +30,6 @@ function Home() {
         <div>
           <CustomImage src={Images.logoImage} height={180} width={180} />
         </div>
-
-        <CustomText textClassName="font-size-xxlarge primary-text font-bold">
-          Coming Soon
-        </CustomText>
       </div>
     </div>
   );
