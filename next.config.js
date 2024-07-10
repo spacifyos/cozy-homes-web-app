00ecec9ba3e457bc68e8962f24d42ffb659d72c2
hz <haozhe@optimumtech.my>
2024-07-10 18:33:37 +0800

    update pwa

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const { i18n } = require("./next-i18next.config");
 const withPWA = require("next-pwa")({
   dest: "public",
-  disable: process.env.PRODUCTION === "development",
 });
 
 const nextConfig = {
