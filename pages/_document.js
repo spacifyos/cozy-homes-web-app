7827a90ad343e4588455886fa249d1d86556c5df
hz <haozhe@optimumtech.my>
2024-07-30 11:08:42 +0800

    update pwa

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,8 @@ export default function Document() {
         <link rel="icon" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" sizes="16x16" href="/favicon-16x16.png" />
 
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+
         <link rel="manifest" href="/manifest.json" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
