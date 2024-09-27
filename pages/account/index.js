0859da252b22b44c3428b5dac6e280e71c18f9fe
hz <haozhe@optimumtech.my>
2024-09-27 17:13:28 +0800

    enhance

--- pages/account/index.js
+++ pages/account/index.js
@@ -308,7 +308,7 @@ const Account = () => {
           </div>
 
           <CustomText textClassName="disable-text font-size-small">
-            {t("account.version")} 1.5.0
+            {t("account.version")} 1.5.1
           </CustomText>
         </div>
 
