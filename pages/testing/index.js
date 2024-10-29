9d83b9113467abcbeb305eb0697e84dee2672b9c
hz <haozhe@optimumtech.my>
2024-10-29 09:26:47 +0800

    integrate iframe

---
+++ pages/testing/index.js
@@ -0,0 +1,9 @@
+const Testing = () => {
+  return (
+    <div>
+      <iframe src="https://staging.renoxpert.my/owner/home" style={{width:"100%", height:"100vh"}} />
+    </div>
+  );
+};
+
+export default Testing;
