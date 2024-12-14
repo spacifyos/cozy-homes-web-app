045820d50e2f355669ac51f03858640bc8077923
hz <haozhe@optimumtech.my>
2024-12-14 13:55:12 +0800

    ui enhance

--- components/Explore/DesktopPromotionSection/index.js
+++ components/Explore/DesktopPromotionSection/index.js
@@ -36,8 +36,8 @@ const DesktopPromotionSection = ({ onClickViewMore, data, loading }) => {
               />
             ))}
           </div>
-        ) : isEmpty(data) ? (
-          <div className="flex justify-center">
+        ) : !isEmpty(data) ? (
+          <div className="flex justify-center h-32 2xl:h-40 xl:h-36 lg:h-36 md:h-32 sm:h-32">
             <CustomEmptyBox
               emptyTitle="Promotion not available now."
               textColor="#FFFFFF"
