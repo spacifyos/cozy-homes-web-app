ad3610d7d2c79c82507570696ab58fc268cf2b79
hz <haozhe@optimumtech.my>
2024-12-24 16:30:57 +0800

    sitemap

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const { i18n } = require("./next-i18next.config");
+
 const withPWA = require("next-pwa")({
   dest: "public",
   disable: process.env.PRODUCTION === "DEVELOPMENT",
