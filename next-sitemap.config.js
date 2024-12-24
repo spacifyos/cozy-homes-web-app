b67f0bb4fa817210f02a972db610a4171119e21b
hz <haozhe@optimumtech.my>
2024-12-24 16:50:12 +0800

    sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'https://www.spacify.asia',
-    generateRobotsTxt: true, // (optional)
-    // ...other options
-}
+  siteUrl: "https://www.spacify.asia",
+  generateRobotsTxt: false, // (optional)
+  exclude: [
+    "/agency/*",
+    "/booking/*/payment-failed",
+    "/booking/*/payment-successful",
+    "/loading",
+    "/payment-failed",
+    "/payment-successful",
+    "/sign-in",
+    "/sign-in/*",
+    "/sign-up/*",
+    "/user/*",
+    "/reset-password",
+    "/401",
+    "/403",
+  ],
+  // ...other options
+};
