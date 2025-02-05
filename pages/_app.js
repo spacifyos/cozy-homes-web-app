d6883a78fb1af8189cc7d0efc05d943eb5e8a1e8
hz <haozhe@optimumtech.my>
2025-02-05 14:16:51 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -132,7 +132,11 @@ function AppContent({ Component, pageProps }) {
         <link rel="manifest" href="/manifest.json" />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
