80fe45a97c58b88cc6ae6216f7ba1f5f13eea212
hz <haozhe@optimumtech.my>
2025-02-07 10:16:15 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -135,8 +135,8 @@ function AppContent({ Component, pageProps }) {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap"
           rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap"
         />
 
         {isEqual(process.env.PRODUCTION, "PRODUCTION") ? (
