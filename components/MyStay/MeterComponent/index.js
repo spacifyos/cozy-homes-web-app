1260be527be339f1548795c3dc6e90122cfef05c
hz <haozhe@optimumtech.my>
2025-02-28 15:10:37 +0800

    update

--- components/MyStay/MeterComponent/index.js
+++ components/MyStay/MeterComponent/index.js
@@ -17,13 +17,6 @@ const MeterComponent = ({ onClickToMeterOverview, item }) => {
       className="meter-container"
       // onClick={() => onClickToMeterOverview(id)}
     >
-      {/*<CustomImage*/}
-      {/*  src={Images.moreIcon}*/}
-      {/*  width={25}*/}
-      {/*  height={25}*/}
-      {/*  className="absolute right-4"*/}
-      {/*/>*/}
-
       <div className="flex items-center pb-1">
         <div className="bg-primary p-3 global-border-radius mb-1 mr-2">
           <CustomImage src={Images.meterIconWhite} className="w-4" />
